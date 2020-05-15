622f58ced11c2fd3dbd666c17f84374ec846618f
curtben1 <60194144+curtben1@users.noreply.github.com>
2020-05-15 15:02:58 +0100

    Update client_B.py

--- client_B.py
+++ client_B.py
@@ -2,29 +2,34 @@ import socket
 import struct
 import sys
 
-master = ()     #add tuple of my pc address + a forwarded port
-me=()           # my ip and a random port as tuple, 3:37 in vid
+master = ("81.151.18.101",7070)     #add tuple of my pc address + a forwarded port
+me=("192.168.1.162",52527)           # my ip and a random port as tuple, 3:37 in vid
 
 
 try:
     sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sockfd.bind(("",0))
-    sockfd.sendto("Hello", master)
+    msg="Hello".encode("ascii")
+    sockfd.sendto("Hello".encode("ascii"), master)
 
 except socket.error:
     print("Failed to create  socket")
     sys.exit
 
 peer_data, addr = sockfd.recvfrom(1024)
-print(peer_data)
+print(peer_data.decode("ascii"))
 
 print("trying to com with peer")
+peer_data=peer_data.decode("ascii")
 peer_ip = peer_data.split(':')[0]
 peer_port = int(peer_data.split(':')[1])
-
-sockfd.sendto("hello from your peer", (peer_ip, peer_port))
+print(peer_ip,peer_port)
+sockfd.sendto("hello from your peer".encode("ascii"), (peer_ip, peer_port))
 
 while True:
+    print("listening")
+
     datarec,sendaddr = sockfd.recvfrom(1024)
-    print("data rec")
+    datarec.decode("ascii")
+    print("data rec",datarec)
     
