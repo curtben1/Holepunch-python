19dabfc751a540b590dacfc053f39afc3dd06b12
curtben1 <60194144+curtben1@users.noreply.github.com>
2020-05-15 14:55:16 +0100

    Update client_A.py

--- client_A.py
+++ client_A.py
@@ -2,27 +2,30 @@ import socket
 import struct
 import sys
 import time
-master = ()     #add tuple of my pc address + a forwarded port
+master = ("81.151.18.101",7070)     #add tuple of my pc address + a forwarded port
 
 
 try:
     sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sockfd.bind(("",0))
-    sockfd.sendto("Hello", master)
+    msg1="Hello".encode("ascii")
+    sockfd.sendto(msg1, master)
 
 except socket.error:
     print("Failed to create  socket")
     sys.exit
 
 peer_data, addr = sockfd.recvfrom(1024)
-print(peer_data)
+print(peer_data.decode("ascii"))
 
-
+peer_data=peer_data.decode("ascii")
 print("Trying to communicate with peer")
 peer_ip = peer_data.split(':')[0]
 peer_port = int(peer_data.split(':')[1])
-
+print(peer_port)
 for i in range(100):
     print("sending",i)
-    sockfd.sendto("Hello from peer client A", (peer_ip,peer_port))
+    msg="Hello from peer client A".encode("ascii")
+    sockfd.sendto(msg, (peer_ip,peer_port))
     time.sleep(0.5)
+    
