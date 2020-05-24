a61d80c97751fac8985f4a043c98e9b5389b91cf
curtben1 <60194144+curtben1@users.noreply.github.com>
2020-05-24 18:30:48 +0100

    added framework for acc use
    started using the tag system sorted my functions out now I just hope socket objects can be passed as parameters

--- client_A.py
+++ client_A.py
@@ -10,11 +10,11 @@ import sys
 import time
 master = ("81.151.18.101",7070)     #home pc with port forwarding so no NAT traversal is necesary
 
-def sendMsg(targetAddr, msg):
+def estCon():
     try:
         sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sockfd.bind(("",0))
-        msg1=targetAddr.encode("ascii")
+        msg1="testString".encode("ascii")
         sockfd.sendto(msg1, master)
 
     except socket.error:
@@ -29,8 +29,23 @@ def sendMsg(targetAddr, msg):
     peer_ip = peer_data.split(':')[0]
     peer_port = int(peer_data.split(':')[1])
     print(peer_port)
+    msg = "connection established"
     for i in range(100):
         print("sending",i)
         msg=msg.encode("ascii")
         sockfd.sendto(msg, (peer_ip,peer_port))
         time.sleep(0.5)
+
+def recvMsg(socket, message, targetIP):
+    master = ("81.151.18.101",7070)     #add tuple of my pc address + a forwarded port
+    me=("192.168.1.162",52527)           
+    message = targetIP + ':' + message
+    message = message.encode("ascii")
+    socket.sendall(message)
+    while True:
+        retValue = socket.recvfrom(1024)
+        retValue = retValue.decode("ascii")
+        tag = retValue.split(':')[0]
+        if tag == "forHost":
+            retValue = retValue.split(':')[1]
+            return retValue
